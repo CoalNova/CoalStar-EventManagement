f2aae2df32dc7819df203e46b702698bc6ac60e6
CGDynamo <CoalNova@protonmail.com>
2022-09-12 15:53:47 -0400

    0.0.2
    Cleaned and added some additional report logging to SDL events.

--- CoalStar-EventManagement.cpp
+++ CoalStar-EventManagement.cpp
@@ -5,16 +5,20 @@
 int main(int arcg, char** argv)
 {
 	int sdlStatus = SDL_Init(SDL_INIT_EVENTS);
+	Coal::LogReport(Coal::Report{ ((sdlStatus != 0) ? Coal::ReportLevel::Error : Coal::ReportLevel::Info), _ReturnAddress(), sdlStatus, 0, "SDL Initialization complete" });
 	SDL_Window* window = SDL_CreateWindow("CoalWindow", 500, 250, 200, 200, SDL_WINDOW_RESIZABLE);
-	Coal::LogReport(Coal::Report{ Coal::ReportLevel::Info , _ReturnAddress(), sdlStatus, 0, "SDL Initialization complete" });
-	bool quit = sdlStatus;
+	Coal::LogReport(Coal::Report{ ((window == nullptr) ? Coal::ReportLevel::Error : Coal::ReportLevel::Info), _ReturnAddress(), (int)window, 0, "SDL Window created" });
+	bool quit = sdlStatus + (window == nullptr);
 	while (!quit)
 	{
 		quit = Coal::RunInputs();
 		quit = Coal::GetInputUp(SDL_SCANCODE_ESCAPE);
 	}
 	SDL_DestroyWindow(window);
+	Coal::LogReport(Coal::Report{ Coal::ReportLevel::Info, _ReturnAddress(), 0,0, "SDL Window closed" });
 	window = nullptr;
+	SDL_Quit();
+	Coal::LogReport(Coal::Report{ Coal::ReportLevel::Info, _ReturnAddress(), 0,0, "SDL Shut down" });
 	Coal::SaveReportLog();
 	return 0;
 }
--- eventmanager.h
+++ eventmanager.h
@@ -13,12 +13,42 @@ namespace Coal
 	/// <returns>Quit Input</returns>
 	bool RunInputs();
 
+	/// <summary>
+	/// Checks SDL events to check if the provided scancode's associated input is depressed
+	/// </summary>
+	/// <param name="scancode">SDL scancode for desired input</param>
+	/// <returns>If input depressed</returns>
 	bool GetInputDown(const SDL_Scancode& scancode);
+	/// <summary>
+	/// Checks SDL events to check if the provided scancode's associated input is held
+	/// </summary>
+	/// <param name="scancode">SDL scancode for desired input</param>
+	/// <returns>If input held</returns>
 	bool GetInputStay(const SDL_Scancode& scancode);
+	/// <summary>
+	/// Checks SDL events to check if the provided scancode's associated input is released
+	/// </summary>
+	/// <param name="scancode">SDL scancode for desired input</param>
+	/// <returns>If input released</returns>
 	bool GetInputUp(const SDL_Scancode& scancode);
+	/// <summary>
+	/// Returns the 'state' of the associated SDL scancode
+	/// </summary>
+	/// <param name="scancode">SDL scancode for desired input</param>
+	/// <returns>The state of the input, either InputState::Down, ::Stay, ::Up, all else ::None</returns>
 	InputState GetInputState(const SDL_Scancode& scancode);
 
+	/// <summary>
+	/// Logs a report
+	/// </summary>
+	/// <param name="report">the report to be logged</param>
 	void LogReport(Report report);
+	/// <summary>
+	/// Prints the Report Log to the console
+	/// </summary>
 	void PrintReportLog();
+	/// <summary>
+	/// Saves the Report Log to the disk, in the current directory
+	/// </summary>
 	void SaveReportLog();
 }
